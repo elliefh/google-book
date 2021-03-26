bad55cefad433fb6a7863be8d9b6f645ab09b43f
elliefh <elliefu18@gmail.com>
2021-03-26 16:21:48 -0700

    set up footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+function Footer() {
+return (
+    <footer>
+        <div class="content has-text-centered">
+            <p>
+                $copy; Ellie Fu-Hinthorn 2021
+            </p>
+        </div>
+    </footer>
+)}
+
+export default Footer;
