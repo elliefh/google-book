369d1ed83c58d156c4bd92f91b992691b316a6d6
elliefh <elliefu18@gmail.com>
2021-03-27 08:28:19 -0700

    added API call

--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -1,7 +1,28 @@
 import SavedHero from '../components/SavedHero';
 import Results from "../components/Results";
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
 
 function Saved() {
+    // Setting our component's initial state
+    const [savedBooks, setBooks] = useState([])
+
+    // Load all books and store them with setBooks
+    useEffect(() => {
+        loadBooks()
+    }, [])
+
+    // Loads all books and sets them to books
+    function loadBooks() {
+        API.getBooks()
+        .then(res => 
+            setBooks(res.data)
+        )
+        .catch(err => console.log(err));
+    };
+
+    console.log(savedBooks);
+    
     return(
         <div>
             <SavedHero/>
--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,10 +1,30 @@
 import SearchHero from '../components/SearchHero';
 import Results from "../components/Results";
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
+
 
 function Search() {
+    // Setting our component's initial state
+    const [query, setQuery] = useState("");
+    const [searchBooks, setBooks] = useState([]);
+
+    // call google books api with 'search' as parameter
+    async function handleFormSubmit(event) {
+        event.preventDefault();
+        const results = await API.searchBook; 
+        setBooks(results.data);
+    }
+
+    console.log(searchBooks);
+
     return(
         <div>
-            <SearchHero/>
+            <SearchHero
+                query={query}
+                setQuery={setQuery}
+                handleFormSubmit={handleFormSubmit}
+            />
             <Results/>
         </div>
     )
