08215d377a140e3bf1b924036047214102b3c918
elliefh <elliefu18@gmail.com>
2021-03-27 08:06:23 -0700

    added bookRoutes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const bookRoutes = require("./Books");
 
 // Book routes
-router.use("/books", bookRoutes);
+router.use("/Books", bookRoutes);
 
 module.exports = router;
