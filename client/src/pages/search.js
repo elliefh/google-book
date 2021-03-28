60dc0b5cb39a1f7ad8a6695043181eca720e99ca
elliefh <elliefu18@gmail.com>
2021-03-28 11:58:15 -0700

    set up handleSave function

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -3,11 +3,11 @@ import { SearchResults } from "../components/Results";
 import React, { useState } from "react";
 import API from "../utils/API";
 
-
 function Search() {
     // Setting our component's initial state
     const [query, setQuery] = useState("");
     const [searchBooks, setBooks] = useState([]);
+    // const [savedBooks, setSavedBooks] = useState([]);
 
     // call google books api with 'search' as parameter
     async function handleFormSubmit(event) {
@@ -16,7 +16,22 @@ function Search() {
         setBooks(results.data.items);
     }
 
-    console.log(searchBooks);
+    const handleSave = (event) => {
+        event.preventDefault();
+        console.log("Selected Book ID: " + event.target.id);
+        let selectedBook = searchBooks.filter(book => book.id === event.target.id);
+        console.log(selectedBook[0]);
+        API.saveBook({
+            title: selectedBook[0].volumeInfo.title,
+            authors: selectedBook[0].volumeInfo.authors,
+            description: selectedBook[0].volumeInfo.description,
+            image: selectedBook[0].volumeInfo.imageLinks.thumbnail,
+            href: selectedBook[0].volumeInfo.infoLink,
+            saved: true
+        })
+            .then(console.log("You saved the following Book: " + selectedBook[0].volumeInfo.title))
+            .catch(err => console.log(err))
+      }
 
     return(
         <div>
@@ -28,12 +43,13 @@ function Search() {
             {
                 searchBooks.map(searchBook => (
                     <SearchResults
-                    key = {searchBook.id}
+                    id = {searchBook.id}
                     title = {searchBook.volumeInfo.title}
                     authors = {searchBook.volumeInfo.authors}
                     description = {searchBook.volumeInfo.description}
                     image = {searchBook.volumeInfo.imageLinks.thumbnail}
                     href = {searchBook.volumeInfo.infoLink}
+                    handleSave = {handleSave}
                     />
                 ))
             }
